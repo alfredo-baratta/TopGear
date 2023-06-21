760d22518222b1f6bd71882398e77e11ecdaa585
Alfredo Baratta <63536128+alfredo-baratta@users.noreply.github.com>
2023-06-21 12:14:35 +0200

    Update CancellaImmagine.java

--- src/main/java/com/servlet/CancellaImmagine.java
+++ src/main/java/com/servlet/CancellaImmagine.java
@@ -24,14 +24,12 @@ public class CancellaImmagine extends HttpServlet {
      */
     public CancellaImmagine() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 	}
 
 	/**
