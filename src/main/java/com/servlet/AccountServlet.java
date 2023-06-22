1a76896167468278d8d48df96f3819c6dd86e1ff
Christian iervasi <c.iervasi@studenti.unisa.it>
2023-06-22 11:46:22 +0200

    Modifica account.jsp e account servlet aggiunte
    ultimata la servlet accountservlet per prendere i dati dell utente da mostrare nella jsp, creata la jsp per la visualizzazione dei dati dell'utente e relativo form per la modifica e l'invio dei nuovi dati alla servlet che dovrà salvarli nel DB

--- src/main/java/com/servlet/AccountServlet.java
+++ src/main/java/com/servlet/AccountServlet.java
@@ -1,9 +1,15 @@
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import Model.DriverManagerConnectionPool;
 
 public class EditAccountServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -12,17 +18,54 @@ public class EditAccountServlet extends HttpServlet {
         HttpSession session = request.getSession(false);
         if (session == null || session.getAttribute("username") == null) {
             // L'utente non è autenticato, reindirizza alla pagina di login o a un'altra pagina di errore
+        	session.invalidate();
             response.sendRedirect("login.jsp");
             return;
         }
 
-        // Recupera i dati dell'account dalla sessione
+        // Recupera la primary key dell'account dalla sessione
         String username = (String) session.getAttribute("username");
-        // recuperare altri dati dal database tramite l'username
+        
+        // recuperare altri dati dal database tramite la primary key
+        
+        try (Connection conn = DriverManagerConnectionPool.getConnection();
+        	
+            String email = "";
+            String password = "";
+            String nome = "";
+            String cognome = "";
+            String indirizzo = "";
+            String citta = "";
+            String datanascita = "";
+            String cellulare = "";
+            String cap = "";	
+        	String cf = "";
+        		
+             PreparedStatement statement = conn.prepareStatement("SELECT * FROM utenti WHERE username = ?")) {
 
+            statement.setString(1, username);
+            try (ResultSet rs = statement.executeQuery()) {
+                if (rs.next()) {
+                	
+                	email = rs.getString("email");
+                	password = rs.getString("password");
+                	nome = rs.getString("nome");
+                	cognome = rs.getString("cognome");
+                	indirizzo = rs.getString("via");
+                	citta = rs.getString("citta");
+                	datanascita = rs.getString("datanascita");
+                	cellulare = rs.getString("telefono");
+                	cap = rs.getString("cap");
+                	cf = rs.getString("cf");
+                }
+            }
+            DriverManagerConnectionPool.releaseConnection(conn);
+        } catch (Exception e) {
+            System.out.println("Errore: " + e.getMessage());
+        }
+        
+        
         // Imposta i dati dell'account come attributi della richiesta
-        request.setAttribute("username", username);
-        request.setAttribute("codicefiscale", cod);
         request.setAttribute("email", email);
         request.setAttribute("password", pwd);
         request.setAttribute("nome", nome);
@@ -32,6 +75,7 @@ public class EditAccountServlet extends HttpServlet {
         request.setAttribute("datanascita", datanascita);
         request.setAttribute("cellulare", cellulare);
         request.setAttribute("cap", cap);
+        request.setAttribute("username", user);
         
 
         // Reindirizza alla JSP per la modifica dei dati dell'account
--- src/main/webapp/modificAccount.jsp
+++ src/main/webapp/modificAccount.jsp
@@ -1,12 +1,80 @@
-<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+pageEncoding="UTF-8" %>
+<%@ include file="utils/sessionCheck.jsp" %>
 <!DOCTYPE html>
 <html>
-<head>
-<meta charset="ISO-8859-1">
-<title>Insert title here</title>
-</head>
-<body>
-
-</body>
+  <head>
+    <meta charset="UTF-8" />
+    <title>Modifica Account</title>
+  </head>
+  <body>
+    <h1>Modifica Account</h1>
+    <form action="salvaModifiche" method="POST">
+      <div>
+        <label>Email:</label>
+        <input type="text" name="email" value= "<%=
+        request.getAttribute("email") %>" readonly />
+      </div>
+      <div>
+        <label>Password:</label>
+        <input type="password" name="password" value= "<%=
+        request.getAttribute("email") %>" readonly />
+      </div>
+      <div>
+        <label>Nome:</label>
+        <input type="text" name="nome" value= "<%= request.getAttribute("email")
+        %>" readonly />
+      </div>
+      <div>
+        <label>Cognome:</label>
+        <input type="text" name="cognome" value= "<%=
+        request.getAttribute("email") %>" readonly />
+      </div>
+      <div>
+        <label>Indirizzo:</label>
+        <input type="text" name="indirizzo" value= "<%=
+        request.getAttribute("email") %>" readonly />
+      </div>
+      <div>
+        <label>Città:</label>
+        <input type="text" name="citta" value= "<%=request.getAttribute("email") %>" readonly />
+      </div>									
+      <div>
+        <label>Data di nascita:</label>
+        <input type="text" name="datanascita" value= "<%=
+        request.getAttribute("email") %>" readonly />
+      </div>
+      <div>
+        <label>Cellulare:</label>
+        <input type="text" name="cellulare" value= "<%=
+        request.getAttribute("email") %>" readonly />
+      </div>
+      <div>
+        <label>CAP:</label>
+        <input type="text" name="cap" value= "<%= request.getAttribute("email")
+        %>" readonly />
+      </div>
+      <div>
+        <label>Username:</label>
+        <input type="text" name="username" value= "<%=
+        request.getAttribute("email") %>" readonly />
+      </div>
+      <input type="submit" value="Salva modifiche" />
+    </form>
+    
+    <script>
+        function abilitaModifica(){
+        document.getElementsByName("email")[0].removeAttribute("readonly");
+        document.getElementsByName("password")[0].removeAttribute("readonly");
+        document.getElementsByName("nome")[0].removeAttribute("readonly");
+        document.getElementsByName("cognome")[0].removeAttribute("readonly");
+        document.getElementsByName("indirizzo")[0].removeAttribute("readonly");
+        document.getElementsByName("citta")[0].removeAttribute("readonly");
+        document.getElementsByName("datanascita")[0].removeAttribute("readonly");
+        document.getElementsByName("cellulare")[0].removeAttribute("readonly");
+        document.getElementsByName("cap")[0].removeAttribute("readonly");
+        document.getElementsByName("username")[0].removeAttribute("readonly");
+      }
+    </script>
+  </body>
 </html>
