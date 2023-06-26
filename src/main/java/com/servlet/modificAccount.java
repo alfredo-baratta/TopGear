3e88075aa6389c14b8739981f1c8ddec0591a7f7
Alfredo Baratta <63536128+alfredo-baratta@users.noreply.github.com>
2023-06-26 18:40:01 +0200

    Update modificAccount.java

--- src/main/java/com/servlet/modificAccount.java
+++ src/main/java/com/servlet/modificAccount.java
@@ -8,11 +8,11 @@ import java.sql.ResultSet;
 import java.util.Date;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
 
 
 @WebServlet("/modificAccount")
