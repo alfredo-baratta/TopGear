542bb18f17617683cedb601c11a1bd038fe4ee2d
Alfredo Napoli <a.napoli109@studenti.unisa.it>
2023-06-28 19:58:09 +0200

    Update
    <joke> ma che cos'è un file PDF ? </joke>
    
    Yea questa è la descrizione so what? シ

--- src/main/java/entities/Ordine.java
+++ src/main/java/entities/Ordine.java
@@ -1,18 +1,25 @@
 package entities;
 
 import java.sql.*;
+
 import Model.DriverManagerConnectionPool;
 
 public class Ordine {
 	private int id;
 	private float totale;
-	private String utente;
+	private String utente_cf;
+	private Date dataPagamento;
 	private boolean type = true;	//true per accessorio, false per personalizzazione
 	
-	public Ordine(int id, float totale, String utente, boolean type) {
+	public Ordine() {
+		
+	}
+	
+	public Ordine(int id, float totale, Date dataPagamento, String utente_cf, boolean type) {
 		this.id = id;
 		this.totale = totale;
-		this.utente = utente;
+		this.dataPagamento = dataPagamento;
+		this.utente_cf = utente_cf;
 		this.type = type;
 	}
 	
@@ -32,12 +39,20 @@ public class Ordine {
 		this.totale = totale;
 	}
 	
-	public String getUtente() {
-		return utente;
+	public Date getDataPagamento() {
+		return dataPagamento;
 	}
 	
-	public void setUtente(String utente) {
-		this.utente = utente;
+	public void setDataPagamento(Date dataPagamento) {
+		this.dataPagamento = dataPagamento;
+	}
+	
+	public String getUtente() {
+		return utente_cf;
+	}
+	
+	public void setUtente(String utente_cf) {
+		this.utente_cf = utente_cf;
 	}
 	
 	public boolean getType() {
@@ -48,13 +63,47 @@ public class Ordine {
 		this.type = type;
 	}
 	
-	//L'idea è prima quello di creare l'ordine eseguendo il costruttore
-	//e successivamente si esegue questa funzione per salvare l'ordine nel DB
-	public Risposta salvaOrdine() {
+	public Risposta salvaOrdine(String cartData) {
 		Risposta r = new Risposta();
 		
+		//Converto i dati in un oggetto Java di tipo Cart che avrà al suo
+		//interno una lista di entities.Accessorio basically
 		
+		Cart cart;  //i know dà errore e sicuramente SonarCloud si è messo a urlare
+					//ma fidatevi che il Bean cart lo creo
+		try {
+			
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
 		
-		return r;
+		//modificare il resto del codice in modo opportuno i guess
+		
+		try (Connection conn = DriverManagerConnectionPool.getConnection()) {
+        	String query = "INSERT INTO ordini (id, totale, data_pagamento, fk_utente) "
+        			+ "VALUES (?, ?, ?, ?)";
+        	PreparedStatement ps = conn.prepareStatement(query);
+        	ps.setInt(1, id);
+	        ps.setFloat(2, totale);
+	        ps.setDate(3, dataPagamento);
+	        ps.setString(4, utente_cf);
+            ps.executeUpdate();
+            
+            setId(id);
+            setTotale(totale);
+            setDataPagamento(dataPagamento);
+            setUtente(utente_cf);
+            
+            DriverManagerConnectionPool.releaseConnection(conn);
+            r.setEsito(true);
+            r.setMessage("OK");
+        }catch (SQLException e) {
+        	System.out.println(e.getMessage());
+        	r.setEsito(false); 
+        	r.setMessage("Ops! Qualcosa e' andato storto. Haha POV: sei me qualsiasi momento della vita");
+        	//TODO: yeayea smelly code, però devo comunque ricordarmi di modificare questo ^ altrimenti piango
+		}
+        return r;
 	}
 }
